960f73edca9bd613f6778198edb15f02c8b97e1b
iagni <inga@bridebook.co.uk>
2018-12-08 17:58:30 +0200

    text add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,11 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        
+                        <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+                        <li><a href="mailto:svetagid@gmail.com" target="_blank" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
-                    <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                    </ul>
+                   
                 </div>
             </div>
         )
